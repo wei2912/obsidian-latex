072f722e5e95323e6440ae269ca90fa4c4e5ff23
Xavier Denis <xldenis@gmail.com>
2020-12-18 07:58:17 -0600

    Patch MathJax loading

--- main.ts
+++ main.ts
@@ -1,44 +1,45 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {  Notice, Plugin } from 'obsidian';
 
-export default class MyPlugin extends Plugin {
+const DEFAULT_PREAMBLE_PATH = "preamble.sty";
+
+export default class JaxPlugin extends Plugin {
+  async reload_preamble() {
+      const file = this.app.vault.getAbstractFileByPath(DEFAULT_PREAMBLE_PATH);
+      console.log(`Loading preamble from ${file}`);
+      const content = await this.app.vault.read(file);
+
+      console.log(content);
+      console.log(typeof MathJax);
+      if (typeof MathJax != 'undefined') {
+          console.log("MATH");
+          await MathJax.tex2chtmlPromise(content);
+      }
+  }
+
 	onload() {
-		console.log('loading plugin');
+    var omg = this;
+    // Hack to extend the MathJax configuration
+    Object.defineProperty(window, 'MathJax', {
+      set(o) {
+        console.log(o);
+        o.startup.typeset = true;
+        o.startup.ready = () => {
+          MathJax.startup.getComponents();
+          MathJax.startup.makeMethods();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+          omg.reload_preamble().then(() => MathJax.config.startup.pageReady()).then(() => MathJax.startup.promiseResolve());
 
-		this.addStatusBarItem().setText('Status Bar Text');
-
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerEvent(this.app.on('codemirror', (cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		}));
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+        }
+        o.startup.pageReady = () => {
+          console.log("FUCK GOD DAMNIT");
+          return MathJax.startup.defaultPageReady();
+          //return omg.reload_preamble().then(MathJax.typesetPromise(MathJax.config.startup.elements));
+        };
+        delete window.MathJax;
+        window.MathJax = o;
+      },
+      configurable: true,
+    });
 	}
 
 	onunload() {
@@ -46,38 +47,3 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange((value) => {
-					console.log('Secret: ' + value);
-				}));
-
-	}
-}
