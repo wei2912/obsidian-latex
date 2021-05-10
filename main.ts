d5bb284ad8a2466252eeb8001863a5ec47392248
Xavier Denis <xldenis@gmail.com>
2021-05-10 21:33:18 +0200

    Minor cleanups

--- main.ts
+++ main.ts
@@ -4,14 +4,6 @@ import { readFileSync } from 'fs';
 const DEFAULT_PREAMBLE_PATH = "preamble.sty";
 
 export default class JaxPlugin extends Plugin {
-  async reload_preamble() {
-      const content = await this.read_preamble();
-
-      if (typeof MathJax != 'undefined') {
-          await MathJax.tex2chtmlPromise(content);
-      }
-  }
-
   async read_preamble () {
       const file = this.app.vault.getAbstractFileByPath(DEFAULT_PREAMBLE_PATH);
       console.log(`Loading preamble from ${file}`);
@@ -27,6 +19,7 @@ export default class JaxPlugin extends Plugin {
           this.read_preamble().then((c) => {
             preludeLoaded = true;
 
+            // Check if MathJax has already loaded and render the preamble
             if (MathJax.tex2chtml == undefined) {
               MathJax.startup.ready = () => {
                 MathJax.startup.defaultReady();
@@ -35,7 +28,8 @@ export default class JaxPlugin extends Plugin {
             } else {
               MathJax.tex2chtml(c);
             }
-            // Refresh the active view
+
+            // Refresh the active view to re-render its math content
             let activeLeaf = window.app.workspace.activeLeaf;
             let preview = activeLeaf.view.previewMode;
             preview.set(preview.get(), true)
